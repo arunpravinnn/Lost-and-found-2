1ece65de3a848116a26da6785266b70ec2fa627d
Abhijith <jithumitnick@gmail.com>
2025-08-10 16:58:48 +0530

    added a readme and some test files

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Backend for Lost and Found Mobile App
+
+## Tech Stack Used:
+    1. Node js 
+    2. Express js 
+    3. Socket.io
+    4. Postgress Supabase
+    5. JWT
+    6. Crypto    
--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -1,9 +1,21 @@
 import express from "express"
-
+import sql from "../db/db"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
 
 const userRouter = express.Router()
 
 userRouter.get('/login',(req,res) => {
+    const AccessTocken = jwt.sign(
+        {"username" : "Admin"},
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : '600s'}
+    )
+    const RefreshToken = jwt.sign(
+        {"username" : "Admin"},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn : '1d'}
+    )
     res.send("This is the signin page for user")
 })
 userRouter.post('/signup',(req,res) => {
@@ -11,16 +23,21 @@ userRouter.post('/signup',(req,res) => {
 })
 
 userRouter.get('/me',(req,res) => {
-    res.send("This is your profile page")
+    // get the email and the password from the current user
 })
 
 userRouter.get('/items',(req,res) => {
-    res.send("This is the place where you can get the list of items lost and found")
+    res.status(200).send({
+        // send the array of list items from the database 
+    })
 })
 
 userRouter.post('/claim',(req,res) =>{
     // get a response code of 200(OK) or 401(unauthorised access)
     const response = req.body;
+    res.status(200).send({
+        'Access Token' : 123456
+    })
 })
 
 
