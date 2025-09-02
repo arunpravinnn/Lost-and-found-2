e3eb40bade680b886c8bc4449c7acbc668808ffd
Chittesh <chittesh249@gmail.com>
2025-09-02 10:38:43 +0530

    deleted unwanted things from the AddItem.tsx page file

--- AdminFrontend/admin-frontend/app/dashboard/AddItems/page.tsx
+++ AdminFrontend/admin-frontend/app/dashboard/AddItems/page.tsx
@@ -3,9 +3,7 @@ import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import axios from "axios";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 
 interface LostItem {
   name: string;
